4189a42ab101d5a48db35f4c2ac9240fe1324e44
ArtemRe <RechembeiArtem@gmail.com>
2022-09-18 22:56:15 +0300

    full

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import user from '../../src/data/user.json';
+import data from '../../src/data/data.json';
+import friends from '../../src/data/friends.json';
+import transactions from '../../src/data/transactions.json';
+
+
+import { Profile } from '../../src/components/Profile/Profile';
+import {Statistics} from '../../src/components/Statistics/Statistics'
+import { FriendList } from '../../src/components/frends/FriendList';
+import { TransactionHistory } from '../../src/components/TransactionHistory/TransactionHistory';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
+    </div>
+  );
+};
